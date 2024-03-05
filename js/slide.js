9b1b16b3e1cb3d0c4a95b49cbfebf370433762b9
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2024-03-05 13:19:59 -0300

    configurações e index dos slides

--- js/slide.js
+++ js/slide.js
@@ -52,10 +52,41 @@ export default class Slide {
         this.onMove = this.onMove.bind(this);
         this.onEnd = this.onEnd.bind(this);
     }
+
+    // slides config
+    slidePosition(slide) {
+        const margin = (this.wrapper.offsetWidth - slide.offsetWidth) / 2;
+        return -(slide.offsetLeft - margin)
+    }
+
+    slidesConfig() {
+        this.slideArray = [...this.slide.children].map((element) => {
+            const position = this.slidePosition(element);
+            return { position, element };
+        });
+        console.log(this.slideArray);
+    }
+
+    slidesIndexNav(index) {
+        const last = this.slideArray.length - 1;
+        this.index = {
+            prev: index ? index -1 : undefined,
+            active: index,
+            next: index === last ? undefined : index + 1
+        }
+    }
+
+    changeSlide(index) {
+        const activiSlide = this.slideArray[index];
+        this.moveSlide(activiSlide.position);
+        this.slidesIndexNav(index);
+        this.dist.finalPosition = activiSlide.position;
+    }
   
     init() {
         this.bindEvents();
         this.addSlideEvents();
+        this.slidesConfig();
         return this;
     }
 }

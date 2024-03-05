6d6c9cdc40e2ce7739cc1c3d058784e799565c9a
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2024-03-05 13:35:21 -0300

    final da navegação prev e nav

--- js/slide.js
+++ js/slide.js
@@ -4,6 +4,10 @@ export default class Slide {
         this.wrapper = document.querySelector(wrapper);
         this.dist = { finalPosition: 0, startX: 0, movement: 0 }
     }
+
+    transition(active) {
+        this.slide.style.transition = active ? 'transform .3s' : '';
+    }
   
     moveSlide(distX) {
         this.dist.movePosition = distX;
@@ -26,6 +30,7 @@ export default class Slide {
             movetype = 'touchmove';
         }
         this.wrapper.addEventListener(movetype, this.onMove);
+        this.transition(false);
     }
   
     onMove(event) {
@@ -38,6 +43,18 @@ export default class Slide {
         const movetype = (event.type === 'mouseup') ? 'mousemove' : 'touchmove'
         this.wrapper.removeEventListener(movetype, this.onMove);
         this.dist.finalPosition = this.dist.movePosition;
+        this.transition(true);
+        this.changeSlideOnEnd();
+    }
+
+    changeSlideOnEnd() {
+        if (this.dist.movement > 120 && this.index.next !== undefined) {
+            this.activeNextSlide();
+        } else if (this.dist.movement < 120 && this.index.prev !== undefined) {
+            this.activePrevSlide();
+        } else {
+            this.changeSlide(this.index.active);
+        }
     }
   
     addSlideEvents() {
@@ -82,9 +99,18 @@ export default class Slide {
         this.slidesIndexNav(index);
         this.dist.finalPosition = activiSlide.position;
     }
-  
+
+    activePrevSlide() {
+        if (this.index.prev !== undefined) this.changeSlide(this.index.prev);
+    }
+    
+    activeNextSlide() {
+        if (this.index.next !== undefined) this.changeSlide(this.index.next);
+    }
+
     init() {
         this.bindEvents();
+        this.transition(true);
         this.addSlideEvents();
         this.slidesConfig();
         return this;
